c77851d0147d22c006a95db61c681ca557488f84
Baptiste C <baptiste.chezaubernard@gmail.com>
2019-12-03 16:41:54 +0100

    update rendu & add viz

--- python/utils.py
+++ python/utils.py
@@ -1,11 +1,7 @@
 import pandas as pd
-import subprocess
-import sys
+import matplotlib.pyplot as plt
+import seaborn as sns
 from neo4j import GraphDatabase
-try:
-    from pip import main as pipmain
-except:
-    from pip._internal.main import main as pipmain
 
 
 
@@ -14,12 +10,6 @@ def readHead(fileName, delimiter=";"):
     return df.head(3)
 
 
-def pip_auto_install():
-    """
-    Automatically installs all requirements if pip is installed.
-    """
-    pipmain(['install', '-r', 'requirements.txt'])
 
 if __name__ == "__main__":
-    pip_auto_install()
     print("Imports réalisés")
