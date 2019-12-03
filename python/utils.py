bef6055f1debaa9ffbaf97ff311439d6938bd8e7
Baptiste C <baptiste.chezaubernard@gmail.com>
2019-12-03 15:47:37 +0100

    update rendu & requests

--- python/utils.py
+++ python/utils.py
@@ -2,18 +2,23 @@ import pandas as pd
 import subprocess
 import sys
 from neo4j import GraphDatabase
+try:
+    from pip import main as pipmain
+except:
+    from pip._internal.main import main as pipmain
+
 
 
 def readHead(fileName, delimiter=";"):
     df = pd.read_csv(fileName, delimiter=delimiter, header=0)
     return df.head(3)
 
+
 def pip_auto_install():
     """
     Automatically installs all requirements if pip is installed.
     """
-    subprocess.call([sys.executable, "-m", "pip",
-                     "install", "-r requirements.txt"])
+    pipmain(['install', '-r', 'requirements.txt'])
 
 if __name__ == "__main__":
     pip_auto_install()
