33e0b9e6c35fdcb7653a989f5e8084bac066c632
Baptiste C <baptiste.chezaubernard@gmail.com>
2019-12-05 15:02:25 +0100

    update rendu : add concerts visualisation

--- python/utils.py
+++ python/utils.py
@@ -3,11 +3,23 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from neo4j import GraphDatabase
 import squarify
-
+import geopandas
 
 def readHead(fileName, delimiter=";"):
     df = pd.read_csv(fileName, delimiter=delimiter, header=0)
     return df.head(3)
+
+def visualiserVilles(df):
+    df=df.copy()
+    print(df.head())
+
+    df = df.dropna()
+    df['lat'] = pd.Series(pd.to_numeric(df['lat'],errors='coerce'))
+    df['lng'] = pd.Series(pd.to_numeric(df['lng'],errors='coerce'))
+    gdf = geopandas.GeoDataFrame(
+    df, geometry=geopandas.points_from_xy(df.lng, df.lat))
+    world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+    gdf.plot(ax=world.plot(), color='red', alpha=0.2, markersize=gdf['compte']*10)
 
 
 
